0521b78bc01ae32ff158327f89503917c936bb33
Masvy <masvy.435@gmail.com>
2023-08-28 15:40:21 +0300

    chore: write a lot of lines of text
    create lexicon(__init__.py, user_lexicon.py) folder
    
    in user_lexicon write the text

---
+++ tgbot/lexiocon/user_lexicon.py
@@ -0,0 +1,9 @@
+USERS: dict[str: str] = {
+    'greetings': 'Добро пожаловать!'
+}
+
+KEYBOARDS: dict[str: str] = {
+    'vacancies': 'Актуальные Вакансии',
+    'company': 'О компании',
+    'employment': 'Трудоустройство'
+}
