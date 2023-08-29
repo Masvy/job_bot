86e78c5b10fb1d9d5c41d9cb8183f273ed82fdbf
Masvy <masvy.435@gmail.com>
2023-08-29 12:51:30 +0300

    chore: remove many handlers

--- tgbot/handlers/start.py
+++ tgbot/handlers/start.py
@@ -5,7 +5,7 @@ from aiogram.filters import CommandStart
 
 from lexiocon.user_lexicon import USERS
 from database.users import read_status, update_status
-from keyboards.user_keyboards import main_menu_kb, back_menu_kb, yes_or_no_kb
+from keyboards.user_keyboards import main_menu_kb, back_menu_kb
 
 start_router: Router = Router()
 
@@ -21,18 +21,6 @@ async def start_bot(update: types.update):
                             reply_markup=main_menu_kb)
 
 
-@start_router.callback_query(F.data == 'vacancies_pressed')
-async def show_vacancies(callback: CallbackQuery,
-                         session_maker: sessionmaker):
-    status = await read_status(callback.from_user.id,
-                               session_maker=session_maker)
-    if status == 0:
-        await callback.message.edit_text(text=USERS['no_access'],
-                                         reply_markup=back_menu_kb)
-    else:
-        await callback.message.edit_text(text='Вакансий на данный момент нет')
-
-
 @start_router.callback_query(F.data == 'company_pressed')
 async def show_company(callback: CallbackQuery,
                        session_maker: sessionmaker):
@@ -45,20 +33,3 @@ async def show_company(callback: CallbackQuery,
                             session_maker=session_maker)
     else:
         pass
-
-
-@start_router.callback_query(F.data == 'employment_pressed')
-async def employ(callback: CallbackQuery):
-    await callback.message.edit_text(text=USERS['employment_info'],
-                                     reply_markup=yes_or_no_kb)
-
-
-@start_router.callback_query(F.data == 'yes_pressed')
-async def warn_user(callback: CallbackQuery):
-    await callback.message.edit_text(text=USERS['agreement'])
-
-
-@start_router.callback_query(F.data == 'no_pressed')
-async def thank_contacting(callback: CallbackQuery):
-    await callback.message.edit_text(text=USERS['refusal'],
-                                     reply_markup=back_menu_kb)
