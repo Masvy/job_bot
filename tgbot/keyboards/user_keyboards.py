49a68ecc8ec4e73aef209dc74e3e617b89d67e1d
Masvy <masvy.435@gmail.com>
2023-08-29 10:56:01 +0300

    chore: create new keyboard objects

--- tgbot/keyboards/user_keyboards.py
+++ tgbot/keyboards/user_keyboards.py
@@ -8,10 +8,28 @@ main_menu_kb: InlineKeyboardMarkup = InlineKeyboardMarkup(
         [
             InlineKeyboardButton(text=KEYBOARDS['vacancies'],
                                  callback_data='vacancies_pressed'),
-            InlineKeyboardButton(text=KEYBOARDS['company'],
+            InlineKeyboardButton(text=KEYBOARDS['about_company'],
                                  callback_data='company_pressed'),
             InlineKeyboardButton(text=KEYBOARDS['employment'],
                                  callback_data='employment_pressed')
+        ]
+    ]
+)
+
+back_menu_kb: InlineKeyboardMarkup = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text=KEYBOARDS['back_menu'],
+                              callback_data='back_menu_pressed')]
+    ]
+)
+
+yes_or_no_kb: InlineKeyboardMarkup = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text=KEYBOARDS['yes'],
+                                 callback_data='yes_pressed'),
+            InlineKeyboardButton(text=KEYBOARDS['no'],
+                                 callback_data='no_pressed')
         ]
     ]
 )
