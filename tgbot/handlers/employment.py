ba37d6c3f63b34083e6c8fef67408eeddb1251d4
Masvy <masvy.435@gmail.com>
2023-08-30 11:30:04 +0300

    feat: create many handlers

--- tgbot/handlers/employment.py
+++ tgbot/handlers/employment.py
@@ -1,7 +1,11 @@
 from aiogram import Router, F
-from aiogram.types import CallbackQuery
+from aiogram.types import CallbackQuery, Message
+from aiogram.filters import StateFilter
+from aiogram.fsm.state import default_state
+from aiogram.fsm.context import FSMContext
 
 from lexiocon.user_lexicon import USERS
+from states.users_states import Questionnaire
 from keyboards.user_keyboards import back_menu_kb, consent_questionnaire_kb, \
                                      yes_or_no_kb
 
@@ -20,9 +24,41 @@ async def warn_user(callback: CallbackQuery):
                                      reply_markup=yes_or_no_kb)
 
 
-@employment_router.callback_query(F.data == 'yes_pressed')
-async def request_name(callback: CallbackQuery):
+@employment_router.callback_query(F.data == 'yes_pressed', StateFilter(default_state))
+async def request_name(callback: CallbackQuery, state: FSMContext):
     await callback.message.edit_text(text=USERS['name_request'])
+    await state.set_state(Questionnaire.name)
+
+
+@employment_router.message(StateFilter(Questionnaire.name))
+async def request_city(message: Message, state: FSMContext):
+    await message.answer(text=USERS['city_request'])
+    await state.set_state(Questionnaire.city)
+
+
+@employment_router.message(StateFilter(Questionnaire.city))
+async def request_vacancies(message: Message, state: FSMContext):
+    await message.answer(text=USERS['vacancies_request'])
+    await state.set_state(Questionnaire.vacancies)
+
+
+@employment_router.message(StateFilter(Questionnaire.vacancies))
+async def request_employment(message: Message, state: FSMContext):
+    await message.answer(text=USERS['type_employment'])
+    await state.set_state(Questionnaire.employment)
+
+
+@employment_router.message(StateFilter(Questionnaire.employment))
+async def request_schedule(message: Message, state: FSMContext):
+    await message.answer(text=USERS['schedule'])
+    await state.set_state(Questionnaire.schedule)
+
+
+@employment_router.message(StateFilter(Questionnaire.schedule))
+async def complete_questionnaire(message: Message, state: FSMContext):
+    await message.answer(text=USERS['end_questionnaire'],
+                         reply_markup=back_menu_kb)
+    await state.clear()
 
 
 @employment_router.callback_query(F.data == 'refusal_pressed')
