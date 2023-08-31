609d53f1c921c9c444cb522f34e0074b5edfb21f
Masvy <masvy.435@gmail.com>
2023-08-31 11:59:35 +0300

    feat: create functions to interact with the db

--- tgbot/database/users.py
+++ tgbot/database/users.py
@@ -4,16 +4,58 @@ from sqlalchemy import select, update
 from database.create_table import User
 
 
-async def read_status(user_id, session_maker: sessionmaker):
+async def read_access(user_id, session_maker: sessionmaker):
     async with session_maker() as session:
         async with session.begin():
-            result = await session.execute(select(User.status).where(User.user_id == user_id))
-            status = result.scalar()
-    return status
+            result = await session.execute(select(User.access).where(User.user_id == user_id))
+            access = result.scalar()
+    return access
 
 
-async def update_status(user_id, session_maker: sessionmaker):
+async def update_access(user_id, session_maker: sessionmaker):
     _session_maker: sessionmaker = session_maker
     async with _session_maker() as session:
         async with session.begin():
-            await session.execute(update(User).where(User.user_id == user_id).values(status=1))
+            await session.execute(update(User).where(User.user_id == user_id).values(access=1))
+
+
+async def update_name(user_id, name, session_maker: sessionmaker):
+    _session_maker: sessionmaker = session_maker
+    async with _session_maker() as session:
+        async with session.begin():
+            await session.execute(update(User).where(User.user_id == user_id).values(name=name))
+
+
+async def update_city(user_id, city, session_maker: sessionmaker):
+    _session_maker: sessionmaker = session_maker
+    async with _session_maker() as session:
+        async with session.begin():
+            await session.execute(update(User).where(User.user_id == user_id).values(city=city))
+
+
+async def update_vacancies(user_id, vacancies, session_maker: sessionmaker):
+    _session_maker: sessionmaker = session_maker
+    async with _session_maker() as session:
+        async with session.begin():
+            await session.execute(update(User).where(User.user_id == user_id).values(vacancies=vacancies))
+
+
+async def update_employment(user_id, employment, session_maker: sessionmaker):
+    _session_maker: sessionmaker = session_maker
+    async with _session_maker() as session:
+        async with session.begin():
+            await session.execute(update(User).where(User.user_id == user_id).values(employment=employment))
+
+
+async def update_schedule(user_id, schedule, session_maker: sessionmaker):
+    _session_maker: sessionmaker = session_maker
+    async with _session_maker() as session:
+        async with session.begin():
+            await session.execute(update(User).where(User.user_id == user_id).values(schedule=schedule))
+
+
+async def update_status(user_id, status, session_maker: sessionmaker):
+    _session_maker: sessionmaker = session_maker
+    async with _session_maker() as session:
+        async with session.begin():
+            await session.execute(update(User).where(User.user_id == user_id).values(status=status))
