16a85d08190bc696bb8471615d8d4a8d6778dbd6
Masvy <masvy.435@gmail.com>
2023-08-31 11:57:56 +0300

    refactor: change table

--- tgbot/database/create_table.py
+++ tgbot/database/create_table.py
@@ -9,8 +9,18 @@ class User(BaseModel):
 
     user_id = Column(BigInteger, unique=True, nullable=False, primary_key=True)
 
-    first_name = Column(VARCHAR(64), unique=False, nullable=False)
-
     user_name = Column(VARCHAR(32), unique=True, nullable=True)
 
-    status = Column(INTEGER, unique=False, default=0)
+    name = Column(VARCHAR(20), unique=False, nullable=True)
+
+    city = Column(VARCHAR(20), unique=False, nullable=True)
+
+    vacancies = Column(VARCHAR(30), unique=False, nullable=True)
+
+    employment = Column(VARCHAR(20), unique=False, nullable=True)
+
+    schedule = Column(VARCHAR(20), unique=False, nullable=True)
+
+    status = Column(VARCHAR())
+
+    access = Column(INTEGER, unique=False, default=0)
