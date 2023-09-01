900217d5d7b71a19562b653b9fd3d929dc6e2374
Masvy <masvy.435@gmail.com>
2023-09-01 11:01:59 +0300

    chore: write new lines of text

--- tgbot/lexiocon/admin_lexicon.py
+++ tgbot/lexiocon/admin_lexicon.py
@@ -1,3 +1,8 @@
 ADMIN: dict[str: str] = {
     'greetings': 'Добро пожаловать в пенель администратора'
 }
+
+KEYBOARDS: dict[str: str] = {
+    'statistics': 'Статистика',
+    'applications': 'Просмотреть заявка'
+}
