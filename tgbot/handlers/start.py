01a32240b55f3f3ceac3887d897f6bc79ed96ec2
Masvy <masvy.435@gmail.com>
2023-09-10 12:33:36 +0300

    feat: create step-by-step loading of buttons

--- tgbot/handlers/start.py
+++ tgbot/handlers/start.py
@@ -1,18 +1,39 @@
 from aiogram import Router, F, types
 from aiogram.filters import CommandStart
+from sqlalchemy.orm import sessionmaker
 
 from lexiocon.user_lexicon import USERS
-from keyboards.user_keyboards import main_menu_kb
+from keyboards.user_keyboards import main_menu_kb_1, main_menu_kb_2, \
+                                     main_menu_kb_3
+from database.users import read_access
 
 start_router: Router = Router()
 
 
 @start_router.callback_query(F.data == 'back_menu_pressed')
 @start_router.message(CommandStart())
-async def start_bot(update: types.update):
-    if isinstance(update, types.CallbackQuery):
-        await update.message.edit_text(text=USERS['main_manu'],
-                                       reply_markup=main_menu_kb)
-    elif isinstance(update, types.Message):
-        await update.answer(text=USERS['greetings'],
-                            reply_markup=main_menu_kb)
+async def start_bot(update: types.update,
+                    session_maker: sessionmaker):
+    access = await read_access(update.from_user.id,
+                               session_maker=session_maker)
+    if access == 0:
+        if isinstance(update, types.CallbackQuery):
+            await update.message.edit_text(text=USERS['main_manu'],
+                                           reply_markup=main_menu_kb_1)
+        elif isinstance(update, types.Message):
+            await update.answer(text=USERS['greetings'],
+                                reply_markup=main_menu_kb_1)
+    elif access == 1:
+        if isinstance(update, types.CallbackQuery):
+            await update.message.edit_text(text=USERS['main_manu'],
+                                           reply_markup=main_menu_kb_2)
+        elif isinstance(update, types.Message):
+            await update.answer(text=USERS['greetings'],
+                                reply_markup=main_menu_kb_2)
+    else:
+        if isinstance(update, types.CallbackQuery):
+            await update.message.edit_text(text=USERS['main_manu'],
+                                           reply_markup=main_menu_kb_3)
+        elif isinstance(update, types.Message):
+            await update.answer(text=USERS['greetings'],
+                                reply_markup=main_menu_kb_3)
