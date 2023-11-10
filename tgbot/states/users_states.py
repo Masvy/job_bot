5b3cd3948be8c20567e79e05058a038a12afa8c9
Masvy <masvy.435@gmail.com>
2023-11-10 12:52:08 +0300

    refactor: completely change user states

--- tgbot/states/users_states.py
+++ tgbot/states/users_states.py
@@ -1,23 +1,46 @@
 from aiogram.fsm.state import State, StatesGroup
 
 
-class Questionnaire(StatesGroup):
-    name = State()
+class Company(StatesGroup):
     city = State()
     vacancies = State()
     employment = State()
     schedule = State()
+    name = State()
+    age = State()
+    experience = State()
+    education = State()
 
 
-class CityRequest(StatesGroup):
-    city = State()
-
-
-class Data(StatesGroup):
+class Vacancies(StatesGroup):
+    vacancies = State()
+    employment = State()
     schedule = State()
-    full_name = State()
+    experience = State()
+    education = State()
+    city = State()
+    name = State()
     age = State()
 
 
-class Questions(StatesGroup):
+class Employments(StatesGroup):
+    city = State()
+    vacancies = State()
+    employment = State()
+    schedule = State()
+    name = State()
+    age = State()
+    experience = State()
+    education = State()
+
+
+class QuestionsCompany(StatesGroup):
+    question = State()
+
+
+class QuestionsVacancies(StatesGroup):
+    question = State()
+
+
+class QuestionsEmployments(StatesGroup):
     question = State()
